b1b784454bf86d7e49a2e19435874873cd4ac94d
Zach Marcus <55953922+zachmarcus7@users.noreply.github.com>
2025-06-05 17:03:15 -0700

    Removed environment variables

--- app/lib/data/groq.ts
+++ app/lib/data/groq.ts
@@ -1,7 +1,7 @@
 import { GroqResponse } from "@/app/lib/types/groq-response";
 
-const GROQ_API_KEY = "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";
-const GROQ_API_BASE = "https://api.groq.com/openai/v1/chat/completions";
+const GROQ_API_KEY = process.env.GROQ_API_KEY;
+const GROQ_API_BASE = process.env.GROQ_API_BASE;
 
 /**
  * Sends a request to the Groq API to attempt to parse the passed transcript into an
@@ -10,7 +10,7 @@ const GROQ_API_BASE = "https://api.groq.com/openai/v1/chat/completions";
  * @returns - Array of GroqResponse objects.
  */
 export async function fetchGroqTranscriptParse(transcript: string): Promise<GroqResponse> {
-  const res = await fetch(GROQ_API_BASE, {
+  const res = await fetch(GROQ_API_BASE as string, {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${GROQ_API_KEY}`,
